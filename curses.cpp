acf5800bb0f970b53c2ff4c7c3f1d70f10dfac04
pradyaheningpertiwi <112565459+pradyaheningpertiwi@users.noreply.github.com>
2022-11-29 22:20:56 +0700

    Add files via upload

---
+++ curses.cpp
@@ -0,0 +1,8 @@
+#include <curses.h>
+using namespace std;
+
+int main(){
+	initscr();
+	printw("kamu gapapa?");
+	
+}
---
+++ peer group.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+using namespace std;
+int main(){
+	int bilangan_sementara,data;
+	double j,i;
+	cin>>j>>i;
+    if(d>data[i])
+	{
+		bilangan_sementara=data[i];
+		data[i] = data[j];
+		data[j] = bilangan_sementara;
+	}
+	
+//	int c,k,m ;
+//	int bilangan;
+//	cin>>bilangan;
+//	c=bilangan%10;
+//	k=((bilangan-c)%100)/10;
+//	m=(bilangan-((k*10)+c))/100;
+//	cout<<m;
+//	string nama;
+//	int uas,uts;
+//	float rata_rata;
+//	
+//	cin>>nama>>uas>>uts;
+//	rata_rata = (uts+uas)/2;
+//	cout<<"nama saya : "<<nama<<endl;
+//	cout<<"nilai rata-rata saya : "<<rata_rata;
+
+//BILANGAN GENAP
+//int angka;
+//cin>>angka;
+//for(int i = 1 ; i<=20 ; i++){
+//	if(i % 2 == 0)
+//	{
+//		cout<<i<<" ";
+//	}	
+//}
+
+
+}
+
+	
